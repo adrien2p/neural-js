60966e7544766671615034132c58065adc50ce99
Adrien de Peretti <adrien.deperetti@dougs.fr>
2017-04-16 20:13:18 +0200

    add fixme in spec file

--- test/network.spec.js
+++ test/network.spec.js
@@ -4,14 +4,13 @@ const fs = require('fs');
 const Neuron = require('../src/neuron');
 const Network = require('../src/network');
 
+// FIXME this is temporary for test the network and show what is computed inside.
 describe('Network', () => {
     it('should create a new network', () => {
         const network = new Network();
         const neuron = new Neuron();
 
         console.log(neuron.process(10));
-
-
 
         const stream = fs.createWriteStream(`./results/newtork-${new Date().toISOString().slice(0, 10)}_${network.id}.json`);
         const networkBuffer = Buffer(JSON.stringify(network.toJSON(), null, 4));
